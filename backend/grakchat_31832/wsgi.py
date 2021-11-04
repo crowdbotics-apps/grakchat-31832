8d63796a2e8a0409f405c9c2702bbf1aa962454c
Crowdbotics <team@crowdbotics.com>
2021-11-04 22:47:46 +0000

    Update models

--- backend/grakchat_31832/wsgi.py
+++ backend/grakchat_31832/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grakchat_31832.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grakchat_31832.settings")
 
 application = get_wsgi_application()
